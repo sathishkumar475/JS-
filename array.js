71ec299f80d7b4c2501cc74e7d0f74e48d7061ca
sathish <118156681+sathishkumar475@users.noreply.github.com>
2024-02-09 07:19:14 +0530

    javascript
    syntax and statement

---
+++ array.js
@@ -0,0 +1,17 @@
+//alert( ' hollow world');
+//alert('open web');
+
+var cars = ["BMW", "VOVLO", "NEW "];
+//document.getElementById ('demo').innerHTML= cars;
+console.log(cars);
+
+let cars = ["BMW", "VOVLO"];  //array operation
+document.getElementById('demo').innerHTML = "cars";
+console.log(cars.length);
+
+
+let BMW = cars[0];
+document.getElementById('demo').innerHTML = 'BMW'; //array item
+
+let len = cars.length;      //array length
+document.getElementById('demo').innerHTML = len;  
